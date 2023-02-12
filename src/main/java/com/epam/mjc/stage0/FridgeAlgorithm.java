5b3591a664cb93694aefa23081a558de6890c76b
Nikolay <124496633+TypicalGrandDad@users.noreply.github.com>
2023-02-12 15:25:45 +0300

    Update FridgeAlgorithm.java

--- src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
+++ src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
@@ -11,6 +11,9 @@ public class FridgeAlgorithm {
      * P.S All methods of interaction with the fridge can be found inside the {@see Fridge} class.
      */
     public void fridgeAlgorithm(Fridge fridge) {
+        var fridge = new FridgeIml();
+        fridgeAlgorithm.fridgeAlgorithm(fridge);
+        var actionsQueue = fridge.getActionsQueue();
 
     }
 }
