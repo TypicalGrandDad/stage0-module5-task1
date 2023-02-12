711f903c7fcf4b68e40e3e4bbade59addf94c106
Nikolay <124496633+TypicalGrandDad@users.noreply.github.com>
2023-02-12 20:41:52 +0300

    Update ArrayTasks.java

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -11,87 +11,76 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-
+        String[] seasons  = {"Winter", "Spring", "Summer", "Autumn"};
+        for (int i = 0; i < 4; i++) {
+            System.out.println(seasons[i]);
+        }
+        return seasons;
     }
-
-    /**
-     * Generate an int[] array of consecutive positive integers
-     * starting at 1 of the given length (length parameter > 0).
-     * <p>
-     * Example:
-     * <p>
-     * length = 1  -> [1]
-     * length = 3  -> [1, 2, 3]
-     * length = 5  -> [1, 2, 3, 4, 5]
-     */
     public int[] generateNumbers(int length) {
-
+        if(length<=0){}
+        else{
+        }
+        int[] arr1 = new int[length];
+        for (int x = 0; x<length;x++){
+            arr1[x] = x+1;
+            System.out.println(arr1[x]);
+        }
+        return arr1;
     }
-
-    /**
-     * Find the sum of all elements of the int[] array.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [1, 3, 5]   -> sum = 9
-     * arr = [5, -3, -4] -> sum = -2
-     */
     public int totalSum(int[] arr) {
+        int sum = 0;
+        int a = arr.length;
+        for (int i = 0; i<a;i++){
+            sum += arr[i];
+        }
+        return sum;
 
     }
-
-    /**
-     * Return the index of the first occurrence of number in the arr array.
-     * If there is no such element in the array, return -1.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [99, -7, 102], number = -7    ->   2
-     * arr = [5, -3, -4],   number = 10    ->  -1
-     */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        int a = arr.length;
+        int thatNumber=0;
+        for(int i = 0 ; i<a; i++){
+            if(arr[i] == number){
+                thatNumber = i;
+            }
+        }
+        return thatNumber;
     }
-
-    /**
-     * Return the new String[] array obtained from the arr array
-     * by reversing the order of the elements.
-     * <p>
-     * Example:
-     * <p>
-     * arr = ["Bob", "Nick"]               -> ["Nick", "Bob"]
-     * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
-     */
     public String[] reverseArray(String[] arr) {
+        int a = arr.length;
+        String thatNumber = "";
+        for (int i = 0; i<a; i++){
+            thatNumber = arr[a-i];
+            arr[i]=arr[a-i];
+            arr[i] = thatNumber;
+
+        }
+        return arr;
 
     }
-
-    /**
-     * Return new int[] array obtained from arr int[] array
-     * by choosing positive numbers only.
-     * P.S. 0 is not a positive number =)
-     * <p>
-     * Example:
-     * <p>
-     * arr = [1,-2, 3]      -> [1, 3]
-     * arr = [-1, -2, -3]   -> []
-     * arr = [1, 2]         -> [1, 2]
-     */
     public int[] getOnlyPositiveNumbers(int[] arr) {
+        int a = arr.length;
+        int thatNumber = 0;
+        int j = 0;
+        for(int i = 0; i<a; i++){
+            if(arr[i]<0){
+                thatNumber = arr[a-i];
+                arr[i]=arr[a-i];
+                arr[i] = thatNumber;
+                j +=1;
+            }
+        }
+        j = a-j;
+        int[]arr1 =new int [j];
+        for (int i = 0; i<j; i ++){
+            arr1[i] = arr[i];
+        }
+        return arr1;
 
     }
-
-    /**
-     * Return a sorted, ragged, two-dimensional int[][] array following these rules:
-     * Incoming one-dimensional arrays must be arranged in ascending order of their length;
-     * numbers in all one-dimensional arrays must be in ascending order.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
-     * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
-     */
     public int[][] sortRaggedArray(int[][] arr) {
+        return arr;
 
     }
 }
